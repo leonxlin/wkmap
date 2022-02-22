f666d49f9df081903017ab80bfc8c824e962236b
Leon Lin <leonxlin@gmail.com>
2022-02-21 23:02:35 -0500

    Add country embedding viewer. Lots of duplicate

--- rollup.config.js
+++ rollup.config.js
@@ -2,14 +2,27 @@ import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import commonjs from "@rollup/plugin-commonjs";
 
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: "bundle.js",
-      format: "iife",
-      name: "wkmap",
-    },
-  ],
-  plugins: [typescript(), commonjs(), resolve()],
-};
+export default [
+  {
+    input: ["src/index.ts"],
+    output: [
+      {
+        file: "bundle.js",
+        format: "iife",
+        name: "wkmap",
+      },
+    ],
+    plugins: [typescript(), commonjs(), resolve()],
+  },
+  {
+    input: ["src/countries.ts"],
+    output: [
+      {
+        file: "countries_bundle.js",
+        format: "iife",
+        name: "countries_bundle",
+      },
+    ],
+    plugins: [typescript(), commonjs(), resolve()],
+  },
+];
